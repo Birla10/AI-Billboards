867302e7fcdd772c96bb35112d18e3bb04b446d1
Birla10 <143983939+Birla10@users.noreply.github.com>
2025-02-28 22:19:29 -0500

    perform dimension reduction on query vector

--- backend/services/perform_ad_search.py
+++ backend/services/perform_ad_search.py
@@ -3,6 +3,8 @@ from data_fetching.currentTime import TimeClassifier
 from ai_analysis.emotion_detection import get_emotion
 from ai_analysis.create_embeddings import CreateEmbeddings
 from database.pinecone_similarity_search import SimilaritySearch
+from sklearn.decomposition import PCA
+import numpy as np
 
 class AdSearch:
     
@@ -18,7 +20,7 @@ class AdSearch:
         current_time = TimeClassifier()
         
         # Get search query
-        query = get_emotion() + " " + "person in "  + weather.get_weather() + " " + current_time.get_month() + " " + current_time.get_time_period()
+        query = "Happy" + " " + "person in "  + weather.get_weather() + " " + current_time.get_month() + " " + current_time.get_time_period()
 
         print(query)
         
@@ -26,7 +28,20 @@ class AdSearch:
         create_embeddings = CreateEmbeddings()
         query_embeddings = create_embeddings.create_obj_embeddings(query)
         
+        print(f"Final embedding shape: {len(query_embeddings)}")
+        
+        reduced_embeddings = self.__reduced_dimension(query_embeddings)
+        
+        print(f"Final embedding shape: {len(reduced_embeddings)}")
+        
         similaritySearch = SimilaritySearch()
         
         # Perform similarity search
-        return similaritySearch.perform_similarity_search(query_embeddings)
+        #return similaritySearch.perform_similarity_search(query_embeddings)
+    
+    def __reduced_dimension(self, embeddings):
+        
+        embeddings_array = np.array(embeddings)
+        
+        pca = PCA(n_components=512)
+        return pca.fit_transform(embeddings_array.reshape(1, -1))
